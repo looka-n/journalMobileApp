5939a4027f51575ece068d1371707b0fece832c6
looka <nguyenlucasv@gmail.com>
2023-12-22 21:17:50 -0800

    Began implementation of navigation between card views

--- App.js
+++ App.js
@@ -1,114 +1,22 @@
-import React, { useState } from 'react';
-import { View, ScrollView, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+// App.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CardViewEdit from './components/CardViewEdit';
+import CardViewFull from './components/CardViewFull';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [text, setText] = useState('');
-  const saveText = () => {
-    console.log('Save')
-  };
-  const navigateBack = () => {
-    console.log('Back')
-  };
-
   return (
-    <View style={styles.page}>
-      <ScrollView>
-        {/* Title */}
-        <Text style={styles.title}>Placeholder title</Text>
-
-        {/* Text entry */}
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.inputText}
-          placeholder='Type here...'
-          placeholderTextColor="#999999"
-          multiline={true}
-          textAlignVertical="top"/>
-        </View>
-
-        {/* Back button */}
-        <Pressable style={styles.buttonBack} onPress={navigateBack}>
-          <Text style={styles.buttonBackText}>
-            Back
-          </Text>
-        </Pressable>
-
-        {/* Save button */}
-        <Pressable style={styles.buttonSave} onPress={saveText}>
-          <Text style={styles.buttonSaveText}>
-            Save
-          </Text>
-        </Pressable>
-      </ScrollView>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="CardViewFull" component={CardViewFull} />
+        <Stack.Screen name="CardViewEdit" component={CardViewEdit} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    backgroundColor: '#131516',
-    paddingTop: 60,
-  },
-  title: {
-    marginLeft: 20,
-
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: '#FFFFFF',
-    letterSpacing: .25,
-  },
-  inputContainer: {
-    flex: 1,
-    alignContent: 'center',
-
-    margin: 20,
-    height: 500,
-    padding: 10,
-    
-    backgroundColor: '#1d2021',
-    borderRadius: 10,
-  },
-  inputText: {
-    fontSize: 15,
-    color: '#FFFFFF',
-  },
-  buttonBack: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 10,
-    elevation: 3,
-    marginLeft: 20,
-    width: 110,
-    backgroundColor: '#1d2021',
-  },
-  buttonBackText: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: '#FFFFFF',
-  },
-  buttonSave: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 10,
-    elevation: 3,
-    marginLeft: 'auto',
-    marginRight: 20,
-    width: 110,
-    backgroundColor: '#1d2021',
-  },
-  buttonSaveText: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: .25,
-    color: '#FFFFFF',
-  }
-});
 
 export default App;
